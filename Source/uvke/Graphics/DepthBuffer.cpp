2215d662c88dea61e7303edf8b4d38644b10e614
eUltrabyte <goszymon1337@gmail.com>
2023-07-28 23:14:38 +0200

    Fixed Depth Buffer

--- Source/uvke/Graphics/DepthBuffer.cpp
+++ Source/uvke/Graphics/DepthBuffer.cpp
@@ -3,65 +3,67 @@
 namespace uvke {
     DepthBuffer::DepthBuffer(Base* base, Surface* surface)
         : m_base(base) {
-        VkImageCreateInfo imageCreateInfo { };
-        imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
-        imageCreateInfo.pNext = nullptr;
-        imageCreateInfo.flags = 0;
-        imageCreateInfo.imageType = VK_IMAGE_TYPE_2D;
-        imageCreateInfo.extent = { surface->GetExtent().width, surface->GetExtent().height, 1 };
-        imageCreateInfo.mipLevels = 1;
-        imageCreateInfo.arrayLayers = 1;
-        imageCreateInfo.format = m_base->GetDepthFormat();
-        imageCreateInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
-        imageCreateInfo.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
-        imageCreateInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-        imageCreateInfo.queueFamilyIndexCount = 0;
-        imageCreateInfo.pQueueFamilyIndices = nullptr;
-        imageCreateInfo.samples = VK_SAMPLE_COUNT_1_BIT;
-        imageCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+        {
+            VkImageCreateInfo imageCreateInfo { };
+            imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+            imageCreateInfo.pNext = nullptr;
+            imageCreateInfo.flags = 0;
+            imageCreateInfo.imageType = VK_IMAGE_TYPE_2D;
+            imageCreateInfo.extent = { surface->GetExtent().width, surface->GetExtent().height, 1 };
+            imageCreateInfo.mipLevels = 1;
+            imageCreateInfo.arrayLayers = 1;
+            imageCreateInfo.format = m_base->GetDepthFormat();
+            imageCreateInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+            imageCreateInfo.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+            imageCreateInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+            imageCreateInfo.queueFamilyIndexCount = 0;
+            imageCreateInfo.pQueueFamilyIndices = nullptr;
+            imageCreateInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+            imageCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
-        UVKE_ASSERT(vkCreateImage(m_base->GetDevice(), &imageCreateInfo, nullptr, &m_image));
-    
-        VkMemoryRequirements memoryRequirements { };
-        vkGetImageMemoryRequirements(m_base->GetDevice(), m_image, &memoryRequirements);
+            UVKE_ASSERT(vkCreateImage(m_base->GetDevice(), &imageCreateInfo, nullptr, &m_image));
+        
+            VkMemoryRequirements memoryRequirements { };
+            vkGetImageMemoryRequirements(m_base->GetDevice(), m_image, &memoryRequirements);
 
-        VkPhysicalDeviceMemoryProperties memoryProperties { };
-        vkGetPhysicalDeviceMemoryProperties(m_base->GetPhysicalDevice(), &memoryProperties);
+            VkPhysicalDeviceMemoryProperties memoryProperties { };
+            vkGetPhysicalDeviceMemoryProperties(m_base->GetPhysicalDevice(), &memoryProperties);
 
-        unsigned int filter = memoryRequirements.memoryTypeBits;
-        VkMemoryPropertyFlags properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
-        unsigned int index = 0;
-        for(auto i = 0; i < memoryProperties.memoryTypeCount; ++i) {
-            if(filter & (1 << i) && (memoryProperties.memoryTypes[i].propertyFlags & properties) == properties) {
-                index = i;
-                break;
+            unsigned int filter = memoryRequirements.memoryTypeBits;
+            VkMemoryPropertyFlags properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+            unsigned int index = 0;
+            for(auto i = 0; i < memoryProperties.memoryTypeCount; ++i) {
+                if(filter & (1 << i) && (memoryProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+                    index = i;
+                    break;
+                }
             }
+
+            VkMemoryAllocateInfo memoryAllocateInfo { };
+            memoryAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+            memoryAllocateInfo.pNext = nullptr;
+            memoryAllocateInfo.allocationSize = memoryRequirements.size;
+            memoryAllocateInfo.memoryTypeIndex = index;
+
+            UVKE_ASSERT(vkAllocateMemory(m_base->GetDevice(), &memoryAllocateInfo, nullptr, &m_imageMemory));
+
+            vkBindImageMemory(m_base->GetDevice(), m_image, m_imageMemory, 0);
+
+            VkImageViewCreateInfo imageViewCreateInfo { };
+            imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            imageViewCreateInfo.pNext = nullptr;
+            imageViewCreateInfo.flags = 0;
+            imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            imageViewCreateInfo.image = m_image;
+            imageViewCreateInfo.format = m_base->GetDepthFormat();
+            imageViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+            imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
+            imageViewCreateInfo.subresourceRange.levelCount = 1;
+            imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+            imageViewCreateInfo.subresourceRange.layerCount = 1;
+
+            UVKE_ASSERT(vkCreateImageView(m_base->GetDevice(), &imageViewCreateInfo, nullptr, &m_imageView));
         }
-
-        VkMemoryAllocateInfo memoryAllocateInfo { };
-        memoryAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
-        memoryAllocateInfo.pNext = nullptr;
-        memoryAllocateInfo.allocationSize = memoryRequirements.size;
-        memoryAllocateInfo.memoryTypeIndex = index;
-
-        UVKE_ASSERT(vkAllocateMemory(m_base->GetDevice(), &memoryAllocateInfo, nullptr, &m_imageMemory));
-
-        vkBindImageMemory(m_base->GetDevice(), m_image, m_imageMemory, 0);
-
-        VkImageViewCreateInfo imageViewCreateInfo { };
-        imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-        imageViewCreateInfo.pNext = nullptr;
-        imageViewCreateInfo.flags = 0;
-        imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-        imageViewCreateInfo.image = m_image;
-        imageViewCreateInfo.format = m_base->GetDepthFormat();
-        imageViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
-        imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
-        imageViewCreateInfo.subresourceRange.levelCount = 1;
-        imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;
-        imageViewCreateInfo.subresourceRange.layerCount = 1;
-
-        UVKE_ASSERT(vkCreateImageView(m_base->GetDevice(), &imageViewCreateInfo, nullptr, &m_imageView));
     }
 
     DepthBuffer::~DepthBuffer() {
@@ -69,6 +71,76 @@ namespace uvke {
             vkDestroyImageView(m_base->GetDevice(), m_imageView, nullptr);
             vkFreeMemory(m_base->GetDevice(), m_imageMemory, nullptr);
             vkDestroyImage(m_base->GetDevice(), m_image, nullptr);
+        }
+    }
+
+    void DepthBuffer::Recreate(Base* base, Surface* surface) {
+        {
+            vkDestroyImageView(m_base->GetDevice(), m_imageView, nullptr);
+            vkFreeMemory(m_base->GetDevice(), m_imageMemory, nullptr);
+            vkDestroyImage(m_base->GetDevice(), m_image, nullptr);
+        }
+
+        {
+            VkImageCreateInfo imageCreateInfo { };
+            imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+            imageCreateInfo.pNext = nullptr;
+            imageCreateInfo.flags = 0;
+            imageCreateInfo.imageType = VK_IMAGE_TYPE_2D;
+            imageCreateInfo.extent = { surface->GetExtent().width, surface->GetExtent().height, 1 };
+            imageCreateInfo.mipLevels = 1;
+            imageCreateInfo.arrayLayers = 1;
+            imageCreateInfo.format = m_base->GetDepthFormat();
+            imageCreateInfo.tiling = VK_IMAGE_TILING_OPTIMAL;
+            imageCreateInfo.usage = VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT;
+            imageCreateInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+            imageCreateInfo.queueFamilyIndexCount = 0;
+            imageCreateInfo.pQueueFamilyIndices = nullptr;
+            imageCreateInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+            imageCreateInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+            UVKE_ASSERT(vkCreateImage(m_base->GetDevice(), &imageCreateInfo, nullptr, &m_image));
+        
+            VkMemoryRequirements memoryRequirements { };
+            vkGetImageMemoryRequirements(m_base->GetDevice(), m_image, &memoryRequirements);
+
+            VkPhysicalDeviceMemoryProperties memoryProperties { };
+            vkGetPhysicalDeviceMemoryProperties(m_base->GetPhysicalDevice(), &memoryProperties);
+
+            unsigned int filter = memoryRequirements.memoryTypeBits;
+            VkMemoryPropertyFlags properties = VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+            unsigned int index = 0;
+            for(auto i = 0; i < memoryProperties.memoryTypeCount; ++i) {
+                if(filter & (1 << i) && (memoryProperties.memoryTypes[i].propertyFlags & properties) == properties) {
+                    index = i;
+                    break;
+                }
+            }
+
+            VkMemoryAllocateInfo memoryAllocateInfo { };
+            memoryAllocateInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+            memoryAllocateInfo.pNext = nullptr;
+            memoryAllocateInfo.allocationSize = memoryRequirements.size;
+            memoryAllocateInfo.memoryTypeIndex = index;
+
+            UVKE_ASSERT(vkAllocateMemory(m_base->GetDevice(), &memoryAllocateInfo, nullptr, &m_imageMemory));
+
+            vkBindImageMemory(m_base->GetDevice(), m_image, m_imageMemory, 0);
+
+            VkImageViewCreateInfo imageViewCreateInfo { };
+            imageViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            imageViewCreateInfo.pNext = nullptr;
+            imageViewCreateInfo.flags = 0;
+            imageViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            imageViewCreateInfo.image = m_image;
+            imageViewCreateInfo.format = m_base->GetDepthFormat();
+            imageViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_DEPTH_BIT;
+            imageViewCreateInfo.subresourceRange.baseMipLevel = 0;
+            imageViewCreateInfo.subresourceRange.levelCount = 1;
+            imageViewCreateInfo.subresourceRange.baseArrayLayer = 0;
+            imageViewCreateInfo.subresourceRange.layerCount = 1;
+
+            UVKE_ASSERT(vkCreateImageView(m_base->GetDevice(), &imageViewCreateInfo, nullptr, &m_imageView));
         }
     }
 
